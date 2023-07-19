0e3814c50c8fdeead2fdb48d284926a9c043f250
Your Name <you@example.com>
2023-07-19 16:03:32 +0700

    Style

--- lua/astrotheme/groups/syntax.lua
+++ lua/astrotheme/groups/syntax.lua
@@ -11,24 +11,24 @@
 local function callback(opts)
   return {
     -- Comment
-    Comment = { fg = C.syntax.comment, bg = C.none, italic = opts.italic_comments }, -- any comment
+    Comment = { fg = C.syntax.comment, bg = C.none, bold = opts.bold_comments, italic = opts.italic_comments }, -- any comment
     -- Constant
-    Constant = { fg = opts.simple_syntax_colors and C.syntax.cyan or C.syntax.yellow, bg = C.none }, -- any constant
-    String = { fg = C.syntax.green, bg = C.none }, -- a string constant: "this is a string"
-    Character = { fg = C.syntax.green, bg = C.none }, -- a character constant: 'c', '\n'
-    Number = { fg = C.syntax.orange, bg = C.none }, -- a number constant: 234, 0xff
-    Boolean = { fg = C.syntax.orange, bg = C.none }, -- a boolean constant: TRUE, false
-    Float = { fg = C.syntax.yellow, bg = C.none }, -- a floating point constant: 2.3e10
+    Constant = { fg = opts.simple_syntax_colors and C.syntax.cyan or C.syntax.yellow, bg = C.none, bold = opts.bold_constants, italic = opts.italic_constants }, -- any constant
+    String = { fg = C.syntax.green, bg = C.none, bold = opts.bold_strings, italic = opts.italic_strings }, -- a string constant: "this is a string"
+    Character = { fg = C.syntax.green, bg = C.nonel, bold = opts.bold_characters, italic = opts.italic_characters }, -- a character constant: 'c', '\n'
+    Number = { fg = C.syntax.orange, bg = C.none, bold = opts.bold_numbers, italic = opts.italic_numbers }, -- a number constant: 234, 0xff
+    Boolean = { fg = C.syntax.orange, bg = C.none, bold = opts.bold_booleans, italic = opts.italic_booleans }, -- a boolean constant: TRUE, false
+    Float = { fg = C.syntax.yellow, bg = C.none, bold = opts.bold_floats, italic = opts.italic_floats }, -- a floating point constant: 2.3e10
     -- Identifier
-    Identifier = { fg = C.syntax.text, bg = C.none }, -- any variable name
-    Function = { fg = C.syntax.blue, bg = C.none }, -- function name (also: methods for classes)
+    Identifier = { fg = C.syntax.text, bg = C.none, bold = opts.bold_identifiers, italic = opts.italic_identifiers }, -- any variable name
+    Function = { fg = C.syntax.blue, bg = C.none, bold = opts.bold_functions, italic = opts.italic_functions }, -- function name (also: methods for classes)
     -- Statement
-    Statement = { fg = C.syntax.purple, bg = C.none }, -- any statement
-    Conditional = { fg = C.syntax.purple, bg = C.none }, -- if, then, else, endif, switch, etc.
-    Repeat = { fg = C.syntax.purple, bg = C.none }, -- for, do, while, etc.
+    Statement = { fg = C.syntax.purple, bg = C.none, bold = opts.bold_statements, italic = opts.italic_statements }, -- any statement
+    Conditional = { fg = C.syntax.purple, bg = C.none, bold = opts.bold_conditionals, italic = opts.italic_conditionals }, -- if, then, else, endif, switch, etc.
+    Repeat = { fg = C.syntax.purple, bg = C.none, bold = opts.bold_repeats, italic = opts.italic_repeats }, -- for, do, while, etc.
     Label = { fg = C.syntax.blue, bg = C.none }, -- case, default, etc.
-    Operator = { fg = opts.simple_syntax_colors and C.syntax.purple or C.syntax.text, bg = C.none }, -- "sizeof", "+", "*", etc.
-    Keyword = { fg = C.syntax.purple, bg = C.none }, -- any other keyword
+    Operator = { fg = opts.simple_syntax_colors and C.syntax.purple or C.syntax.text, bg = C.none, bold = opts.bold_operators, italic = opts.italic_operators }, -- "sizeof", "+", "*", etc.
+    Keyword = { fg = C.syntax.purple, bg = C.none, bold = opts.bold_keywords, italic = opts.italic_keywords }, -- any other keyword
     Exception = { fg = C.syntax.purple, bg = C.none }, -- try, catch, throw
     -- Preproc
     PreProc = { fg = C.syntax.yellow, bg = C.none }, -- generic Preprocessor
@@ -37,7 +37,7 @@ local function callback(opts)
     Macro = { fg = C.syntax.orange, bg = C.none }, -- same as Define
     PreCondit = { fg = C.syntax.blue, bg = C.none }, -- preprocessor #if, #else, #endif, etc.
     -- Type
-    Type = { fg = C.syntax.blue, bg = C.none }, -- int, long, char, etc.
+    Type = { fg = C.syntax.blue, bg = C.none, bold = opts.bold_types, italic = opts.italic_types }, -- int, long, char, etc.
     StorageClass = { fg = C.syntax.blue, bg = C.none }, -- static, register, volatile, etc.
     Structure = { fg = C.syntax.yellow, bg = C.none }, -- struct, union, enum, etc.
     Typedef = { fg = C.syntax.yellow, bg = C.none }, -- A typedef
